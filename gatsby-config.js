bb79254ff7f1d37d5b94567ddf42c2d5575b72ad
Massimo Bortolato <massimo.bortolato@gmail.com>
2021-10-15 15:35:36 +0200

    c1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Massimo Bortolato",
-  },
-  plugins: [],
-};
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "Massimo Bortolato",
+    },
+    plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+}
